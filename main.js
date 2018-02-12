92e01587d4e94bc61f4886a4214cb9bc23c509b1
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-02-12 19:24:42 +0100

    Cleanup

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ require('source-map-support').install()
 const program = require('commander');
 
 const run = require('./lib/run').run;
+const version = require('./package.json').version;
+
 
 program
-  .arguments('[options] <file> [languages...]')
-  .option('-o, --outputdir <outputdir>', 'The output directory')
-  .action(function(file, outputdir, languages) {
-    run(file, languages || ['python'], outputdir);
+  .version(version)
+  .option('-o, --outputdir [outputdir]', 'The output directory')
+  .arguments('<file> [languages...]')
+  .action(function(file, languages, cmd) {
+    run(file, languages || ['python'], cmd.outputdir);
   })
-  .parse(process.argv);
+  .parse(process.argv)
