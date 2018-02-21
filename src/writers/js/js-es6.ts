8af3c9e2dd0d51a051668a4147de85904bedee18
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-02-21 18:02:11 +0100

    Switch JS writers to be template based

--- src/writers/js/js-es6.ts
+++ src/writers/js/js-es6.ts
@@ -5,12 +5,8 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 
 import {
-  Writer
-} from '../base';
-
-import {
-  Parser
-} from '../../parsers';
+  TemplateWriter, TemplateState
+} from '../template';
 
 import {
   IWidget, hasWidgetRef, Attributes
@@ -21,155 +17,75 @@ import {
 } from './utils';
 
 
-//  The common header to put at the top of each generated file
-export
-const HEADER = `
-import {
-  WidgetModel, DOMWidgetModel,
-  WidgetView, DOMWidgetView,
-  unpack_models
-} from '@jupyter-widgets/base';
-
-import {
-  data_union_serialization, array_serialization
-} from 'jupyter-dataserializers';
-`;
-
-//  The indentation to use
-export
-const INDENT = '  ';
-
-
-
 /**
  * Javascript ES6 code writer.
  */
 export
-class JSES6Writer extends Writer {
-  constructor(output: string) {
-    super(output);
-    if (!this.outputMultiple) {
-      // Clear the file
-      fs.writeFileSync(this.output, '');
+class JSES6Writer extends TemplateWriter {
+  constructor(output: string, options: Partial<TemplateWriter.IOptions> = {}) {
+    if (!options.template) {
+      options.template = path.resolve(__dirname, '../../../templates/js-es6.hbs');
     }
+    super(output, options);
   }
 
-  /**
-   * Process the widget definition
-   */
-  onWidget(sender: Parser, data: IWidget): void {
-    const lines = this.genLines(sender, data);
-
-    if (this.outputMultiple) {
-        let fname = path.join(this.output, `${name}.js`);
-        this.modules.push(name);
-        fs.writeFileSync(fname, lines.join('\n'));
-      } else {
-        fs.appendFileSync(this.output, lines.join('\n'));
+  transformState(data: TemplateState): TemplateState {
+    data.serializers = [];
+    data.widgets = data.widgets.map((widget) => {
+      let {inherits, properties} = widget;
+      if (inherits.length > 1) {
+        console.warn(`Cannot use multiple inheritance for ${name} with JS.` +
+          `Dropping ancestors: ${inherits.slice(1)}`);
       }
-  }
-
-  /**
-   * Convert the widget definition into code lines.
-   */
-  genLines(sender: Parser, data: IWidget, header?: string): string[] {
-    if (header === undefined) {
-      header = HEADER;
-    }
-    const lines: string[] = [];
-    let {name, inherits, properties} = data;
-
-    if (this.outputMultiple || this.firstWidget) {
-      this.firstWidget = false;
-      lines.push(...header.split('\n'));
-    }
-
-    inherits = inherits || ['Widget'];
-    if (inherits.length > 1) {
-      console.warn(`Cannot use multiple inheritance for ${name} with JS.` +
-        `Dropping ancestors: ${inherits.slice(1)}`);
-    }
-    if (this.outputMultiple && inherits) {
-      let refs = sender.widgetNames.intersection(inherits);
-      console.debug(`${name} depends on: ${refs}`);
-      refs.forEach((ref) => {
-        lines.push(`import { ${ref}Model } from './${ref}';`);
-      });
-    }
-    lines.push(
-      '', '', // add some empty lines
-      'export',
-      `class ${name}Model extends ${inherits[0]}Model {`,
-      '',
-    )
-    let serializers: {[key: string]: string} = {};
-    lines.push(
-      `${INDENT}defaults() {`,
-      `${INDENT}${INDENT}return {...super.defaults(), ...{`,
-    );
-    if (properties) {
-      for (let key of Object.keys(properties)) {
-        lines.push(
-          `${INDENT}${INDENT}${INDENT}${key}: ${getDefaultValue(properties[key])},`);
-        if (Attributes.isDataUnion(properties[key])) {
-          serializers[key] = 'data_union_serialization';
-        } else if (Attributes.isNDArray(properties[key])) {
-          serializers[key] = 'array_serialization';
-        } else if (hasWidgetRef(properties[key])) {
-          serializers[key] = '{ deserialize: unpack_models }';
+      if (properties) {
+        for (let key of Object.keys(properties)) {
+          if (Attributes.isDataUnion(properties[key])) {
+            data.serializers[key] = 'data_union_serialization';
+          } else if (Attributes.isNDArray(properties[key])) {
+            data.serializers[key] = 'array_serialization';
+          } else if (hasWidgetRef(properties[key])) {
+            data.serializers[key] = '{ deserialize: unpack_models }';
+          }
         }
       }
-    }
-    lines.push(
-      `${INDENT}${INDENT}}}`,
-      `${INDENT}}`,
-      ``,
-    );
-
-    if (Object.keys(serializers).length) {
-      lines.push(
-        ...this.genSerializers(inherits, serializers),
-      );
-    }
-    lines.push('}');
-    lines.push('')  // add an empty line at end
-    return lines;
+      return {
+        ...widget,
+        properties: properties ? Object.keys(properties).reduce((res, key) => {
+          let attr = properties![key];
+          res[key] = {
+            ...attr,
+            default: getDefaultValue(attr),
+          }
+          return res;
+        }, {} as TemplateState) : properties,
+        inherits: inherits.slice(0, 1),
+      };
+    });
+    return data;
   }
 
-  /**
-   * Generate lines for a static serializers attribute
-   * @param inherits The ancestors of the widget
-   * @param serializers Map of serializers to use
-   */
-  protected genSerializers(inherits: string[], serializers: {[key: string]: string}): string[] {
-    return [
-      `${INDENT}serializers = {`,
-      `${INDENT}${INDENT}...${inherits[0]}Model.serializers,`,
-      ...Object.keys(serializers).map((key) => {
-        return `${INDENT}${INDENT}${key}: ${serializers[key]},`;
-      }),
-      `${INDENT}};`,
-    ];
+  filenameForWidget(widget: IWidget): string {
+    return path.join(this.output, `${widget.name}.js`);
+  }
+
+  filenameForIndex(): string {
+    return path.join(this.output, `index.js`);
   }
 
   /**
    * Called when all widgets are parsed. Writes index.js if appropriate.
    */
   finalize(): Promise<void> {
-    if (this.outputMultiple) {
-      // Write init file for directory output
-      let fname = path.join(this.output, `index.js`);
-      let lines = this.modules.map((name) => {
-        return `export { ${name}Model } from './${name}';`;
-      });
-      lines.push('');  // add an empty line at end
-      return fs.writeFile(fname, lines.join('\n'));
-    }
-    return Promise.resolve();
+    return Promise.all([super.finalize(), Promise.resolve().then(() =>{
+      if (this.outputMultiple) {
+        // Write init file for directory output
+        let fname = this.filenameForIndex();
+        let lines = this.modules.map((name) => {
+          return `export { ${name}Model } from './${name}';`;
+        });
+        lines.push('');  // add an empty line at end
+        return fs.writeFile(fname, lines.join('\n'));
+      }
+    })]).then(() => {});
   }
-
-  /**
-   * An array of all the module names written to disk if multi-output.
-   */
-  modules: string[] = [];
 }
--- src/writers/js/ts.ts
+++ src/writers/js/ts.ts
@@ -1,41 +1,18 @@
 
-
-import * as fs from 'fs-extra';
-
 import * as path from 'path';
 
 import {
-  JSES6Writer, INDENT, HEADER as ES6_HEADER
+  JSES6Writer
 } from './js-es6';
 
 import {
-  Parser
-} from '../../parsers';
+  TemplateWriter
+} from '../template';
 
 import {
   IWidget
 } from '../../core';
 
-
-
-const HEADER = `${ES6_HEADER}
-import {
-  ManagerBase
-} from '@jupyter-widgets/base';
-
-
-/**
- * Type declaration for general widget serializers.
- *
- * Declared in lieu of proper interface in jupyter-widgets.
- */
-export interface ISerializers {
-  [key: string]: {
-      deserialize?: (value?: any, manager?: ManagerBase<any>) => any;
-      serialize?: (value?: any, widget?: WidgetModel) => any;
-  };
-}
-`;
 
 
 /**
@@ -45,57 +22,18 @@ export interface ISerializers {
  */
 export
 class TSWriter extends JSES6Writer {
-
-  /**
-   * Process the widget definition
-   */
-  onWidget(sender: Parser, data: IWidget): void {
-    const lines = this.genLines(sender, data, HEADER);
-    let {name} = data;
-
-    if (this.outputMultiple) {
-        let fname = path.join(this.output, `${name}.ts`);
-        this.modules.push(name);
-        fs.writeFileSync(fname, lines.join('\n'));
-      } else {
-        fs.appendFileSync(this.output, lines.join('\n'));
-      }
-  }
-
-  /**
-   * Generate lines for a static serializers attribute
-   * @param inherits The ancestors of the widget
-   * @param serializers Map of serializers to use
-   */
-  protected genSerializers(inherits: string[], serializers: {[key: string]: string}): string[] {
-    return [
-      `${INDENT}static serializers = {`,
-      `${INDENT}${INDENT}...${inherits[0]}Model.serializers,`,
-      ...Object.keys(serializers).map((key) => {
-        return `${INDENT}${INDENT}${key}: ${serializers[key]},`;
-      }),
-      `${INDENT}};`,
-    ];
-  }
-
-  /**
-   * Called when all widgets are parsed. Writes index.ts if appropriate.
-   */
-  finalize(): Promise<void> {
-    if (this.outputMultiple) {
-      // Write init file for directory output
-      let fname = path.join(this.output, `index.ts`);
-      let lines = this.modules.map((name) => {
-        return `export { ${name}Model } from './${name}';`;
-      });
-      lines.push('');  // add an empty line at end
-      return fs.writeFile(fname, lines.join('\n'));
+  constructor(output: string, options: Partial<TemplateWriter.IOptions> = {}) {
+    if (!options.template) {
+      options.template = path.resolve(__dirname, '../../../templates/ts.hbs');
     }
-    return Promise.resolve();
+    super(output, options);
   }
 
-  /**
-   * An array of all the module names written to disk if multi-output.
-   */
-  modules: string[] = [];
+  filenameForWidget(widget: IWidget): string {
+    return path.join(this.output, `${widget.name}.ts`);
+  }
+
+  filenameForIndex(): string {
+    return path.join(this.output, `index.ts`);
+  }
 }
